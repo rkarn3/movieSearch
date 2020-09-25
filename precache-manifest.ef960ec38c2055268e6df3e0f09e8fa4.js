6382fc6a1de4218235e88c8e3b2ad882495411f9
Roshan Kumar Karn <rkkarn1@gmail.com>
2020-09-25 12:41:23 +0530

    Updates

---
+++ precache-manifest.ef960ec38c2055268e6df3e0f09e8fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08f857d37a8814967eb9903d1d187ca",
+    "url": "/movieSearch/index.html"
+  },
+  {
+    "revision": "4f4abdb0a6972330c476",
+    "url": "/movieSearch/static/css/main.9f617a00.chunk.css"
+  },
+  {
+    "revision": "234acde516ed13b8957c",
+    "url": "/movieSearch/static/js/2.36adfc98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movieSearch/static/js/2.36adfc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4abdb0a6972330c476",
+    "url": "/movieSearch/static/js/main.12ae34c2.chunk.js"
+  },
+  {
+    "revision": "3e46a461d71bda7c9dcd",
+    "url": "/movieSearch/static/js/runtime-main.2005d457.js"
+  }
+]);
