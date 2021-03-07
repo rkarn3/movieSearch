92dae6099492fa9dfda8b5ce235f1fd6804d9f46
Roshan Kumar Karn <rkkarn1@gmail.com>
2021-03-07 18:03:19 +0530

    changed the home page

--- src/App.js
+++ src/App.js
@@ -1,55 +1,98 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Search from './components/Search';
 import Favourites from './components/Favourites';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  const [favourites, setFavourites] = useState([]);
+	const [favourites, setFavourites] = useState([]);
+	const [allMovies, setAllMovies] = useState([]);
 
-  const addFavourite = (favourite) => {
-    setFavourites([...favourites, favourite]);
-    localStorage.setItem(
-      'favourites',
-      JSON.stringify([...favourites, favourite])
-    );
-  };
-  return (
-    <Router>
-      <nav className="navbar navbar-light bg-light">
-        <Link to="/movieSearch" className="navbar-brand">
-          Home
-        </Link>
+	const addFavourite = (favourite) => {
+		setFavourites([...favourites, favourite]);
+		localStorage.setItem(
+			'favourites',
+			JSON.stringify([...favourites, favourite])
+		);
+	};
 
-        <form className="form-inline">
-          <Link to="/Favourites">
-            <button
-              className="btn btn-outline-success my-2 my-sm-0"
-              type="submit"
-            >
-              Favourites
-            </button>
-          </Link>
-        </form>
-      </nav>
-      <Switch>
-        <Route
-          path="/movieSearch"
-          exact
-          component={() => <Search addFavourite={addFavourite} />}
-        />
-        <Route
-          path="/favourites"
-          exact
-          component={() => (
-            <Favourites
-              favourites={JSON.parse(localStorage.getItem('favourites'))}
-            />
-          )}
-        />
-      </Switch>
-    </Router>
-  );
+	useEffect(() => {
+		async function showMovies() {
+			const url = `https://www.omdbapi.com/?apikey=a57d910e&s=mission`;
+			try {
+				const res = await fetch(url);
+				const data = await res.json();
+				setAllMovies(data.Search);
+			} catch {
+				alert("Movies Couldn't be Loaded");
+			}
+		}
+		showMovies();
+	}, []);
+
+	return (
+		<>
+			<Router>
+				<nav className='navbar navbar-light bg-light'>
+					<Link to='/movieSearch' className='navbar-brand'>
+						Home
+					</Link>
+
+					<form className='form-inline'>
+						<Link to='/Favourites'>
+							<button
+								className='btn btn-outline-success my-2 my-sm-0'
+								type='submit'>
+								Favourites
+							</button>
+						</Link>
+					</form>
+				</nav>
+				<Switch>
+					<Route
+						path='/movieSearch'
+						exact
+						component={() => <Search addFavourite={addFavourite} />}
+					/>
+					<Route
+						path='/favourites'
+						exact
+						component={() => (
+							<Favourites
+								favourites={JSON.parse(localStorage.getItem('favourites'))}
+							/>
+						)}
+					/>
+				</Switch>
+			</Router>
+			<div className='container'>
+				<div className='row'>
+					{allMovies ? (
+						allMovies.map((allMovie) => (
+							<div className='col-md-4' key={allMovie.imdbID}>
+								<div
+									className='card mb-2 mt-2'
+									style={{ width: '20rem', height: '40rem' }}>
+									<img
+										src={allMovie.Poster}
+										className='card-img-top'
+										alt='...'
+									/>
+
+									<div className='card-body'>
+										<h5 className='card-title'>{allMovie.Title}</h5>
+										<p className='card-text'>Released On: {allMovie.Year}</p>
+									</div>
+								</div>
+							</div>
+						))
+					) : (
+						<h1>Sorry</h1>
+					)}
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,93 +1,90 @@
 import React, { useState } from 'react';
 
 const Search = (props) => {
-  const [query, setQuery] = useState('');
-  const [movies, setMovies] = useState([]);
-  const [type, setType] = useState('');
+	const [query, setQuery] = useState('');
+	const [movies, setMovies] = useState([]);
+	const [type, setType] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const url = `https://www.omdbapi.com/?apikey=a57d910e&s=${query}&type=${type}`;
-    try {
-      const res = await fetch(url);
-      const data = await res.json();
-      setMovies(data.Search);
-    } catch (err) {
-      alert('Cannot find movie');
-    }
-  };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const url = `https://www.omdbapi.com/?apikey=a57d910e&s=${query}&type=${type}`;
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+			setMovies(data.Search);
+		} catch (err) {
+			alert('Cannot find movie');
+		}
+	};
 
-  return (
-    <>
-      <div className="container">
-        <div className="row mt-3 ">
-          <form className="form-inline" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              className="form-control col-md-6"
-              name="query"
-              size="150"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-            />
-            <select
-              name="type"
-              value={type}
-              onChange={(evt) => {
-                setType(evt.target.value);
-              }}
-              className="input-group-append sel"
-            >
-              <option value="">All</option>
-              <option value="movie">Movie</option>
-              <option value="series">Series</option>
-              <option value="episode">Episode</option>
-            </select>
-            <button
-              type="submit"
-              className="btn btn-info input-group-append"
-              // onClick={handleSubmit}
-            >
-              Search
-            </button>
-          </form>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          {movies ? (
-            movies.map((movie) => (
-              <div className="col-md-4" key={movie.imdbID}>
-                <div
-                  className="card mb-2 mt-2"
-                  style={{ width: '20rem', height: '40rem' }}
-                >
-                  <img src={movie.Poster} className="card-img-top" alt="..." />
+	return (
+		<>
+			<div className='container'>
+				<div className='row mt-3 '>
+					<form className='form-inline' onSubmit={handleSubmit}>
+						<input
+							type='text'
+							className='form-control col-md-6'
+							name='query'
+							size='150'
+							value={query}
+							onChange={(e) => setQuery(e.target.value)}
+						/>
+						<select
+							name='type'
+							value={type}
+							onChange={(evt) => {
+								setType(evt.target.value);
+							}}
+							className='input-group-append sel'>
+							<option value=''>All</option>
+							<option value='movie'>Movie</option>
+							<option value='series'>Series</option>
+							<option value='episode'>Episode</option>
+						</select>
+						<button
+							type='submit'
+							className='btn btn-info input-group-append'
+							// onClick={handleSubmit}
+						>
+							Search
+						</button>
+					</form>
+				</div>
+			</div>
+			<div className='container'>
+				<div className='row'>
+					{movies ? (
+						movies.map((movie) => (
+							<div className='col-md-4' key={movie.imdbID}>
+								<div
+									className='card mb-2 mt-2'
+									style={{ width: '20rem', height: '40rem' }}>
+									<img src={movie.Poster} className='card-img-top' alt='...' />
 
-                  <div className="card-body">
-                    <h5 className="card-title">{movie.Title}</h5>
-                    <p className="card-text">Released On: {movie.Year}</p>
-                    <button
-                      className="btn btn-primary"
-                      onClick={(e) => {
-                        e.preventDefault();
-                        alert('Added to favourites');
-                        props.addFavourite(movie);
-                      }}
-                    >
-                      Add to Favourites
-                    </button>
-                  </div>
-                </div>
-              </div>
-            ))
-          ) : (
-            <h1>Invalid movie name</h1>
-          )}
-        </div>
-      </div>
-    </>
-  );
+									<div className='card-body'>
+										<h5 className='card-title'>{movie.Title}</h5>
+										<p className='card-text'>Released On: {movie.Year}</p>
+										<button
+											className='btn btn-primary'
+											onClick={(e) => {
+												e.preventDefault();
+												alert('Added to favourites');
+												props.addFavourite(movie);
+											}}>
+											Add to Favourites
+										</button>
+									</div>
+								</div>
+							</div>
+						))
+					) : (
+						<h1>Invalid movie name</h1>
+					)}
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Search;
